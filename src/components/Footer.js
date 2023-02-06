72c57ddc299bb0bca9d4919727b152867c87b011
Pratik <pratikmandal3001@gmail.com>
2023-02-06 19:06:43 +0530

    Footer Updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,35 +5,7 @@ export default class Footer extends Component {
     return (
       <div>
         <footer class="text-center text-lg-start bg-light text-muted">
-          <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-            <div class="container text-center text-md-start mt-5">
-              <div class="row mt-3">
-                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                  <h6 class="text-uppercase fw-bold mb-4">
-                    <i class="fas fa-gem me-3"></i>Newsly
-                  </h6>
-                  <p>Get your daily dose of News here.</p>
-                </div>
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                  <h6 class="text-uppercase fw-bold mb-4">Developer Contact</h6>
-                  <p>
-                    <i class="fas fa-home me-3"></i> Kolkata, 700001, WB
-                  </p>
-                  <p>
-                    <i class="fas fa-envelope me-3"></i>
-                    info@newsly.com
-                  </p>
-                  <p>
-                    <i class="fas fa-phone me-3"></i> + 91 9619320623
-                  </p>
-                  <p>
-                    <i class="fas fa-print me-3"></i> + 91 8963258444
-                  </p>
-                </div>
-              </div>
-            </div>
-          </section>
-          <div class="text-center p-4">© 2023 Newsly All Rights Reserved.</div>
+          <div class="text-center p-4">© 2023 Newsly</div>
         </footer>
       </div>
     );
--- src/components/News.js
+++ src/components/News.js
@@ -13,19 +13,18 @@ export class News extends Component {
     }
 
     async componentDidMount(){
-      
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53ba08424a8148c49de4f2d452d4dc8b&page=1&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
       let parsedData = await data.json();
       this.setState({
-                      articles: parsedData.articles, 
-                      totalResults: parsedData.totalResults,
-                      loading: false});
+        articles: parsedData.articles, 
+        totalResults: parsedData.totalResults,
+        loading: false
+      });
     }
 
     handlePrevClick = async() => {
-      
       let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53ba08424a8148c49de4f2d452d4dc8b&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
       this.setState({loading: true});
       let data = await fetch(url);
